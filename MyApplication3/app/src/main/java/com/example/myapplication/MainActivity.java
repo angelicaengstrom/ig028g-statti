a014d314e8efbd57a6bd98d522c0b7af120a27e3
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-03-20 13:53:25 +0100

    Update MainActivity.java

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,28 +7,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        loginBtn = (Button) findViewById(R.id.loginBtn);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                openAdd();
-            }
-        });
-    }
-
-    private void openAdd() {
-        Intent intent = new Intent(this, Add.class);
-        startActivity(intent);
-    }
-
-    private void startActivity(Intent intent) {
     }
 
 }
