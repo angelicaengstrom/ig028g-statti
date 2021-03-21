34b69665aa5c606efea6cc62a562d7bb7eb7dbc9
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-03-21 14:40:02 +0100

    Update Graph.java

--- MyApplication3/app/src/main/java/com/example/myapplication/Graph.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Graph.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class Graph extends AppCompatActivity {
 
@@ -11,5 +13,23 @@ public class Graph extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        Button menu_calenderBtn = findViewById(R.id.menu_calender);
+        menu_calenderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), Calender.class);
+                startActivity(startIntent);
+            }
+        });
+
+        Button menu_addBtn = findViewById(R.id.menu_add);
+        menu_addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), Add.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
