c182501e86d245f0dc293ad772b5516ee2d1aeea
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-03-21 14:40:06 +0100

    Update Calender.java

--- MyApplication3/app/src/main/java/com/example/myapplication/Calender.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Calender.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class Calender extends AppCompatActivity {
 
@@ -11,5 +13,23 @@ public class Calender extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
+
+        Button menu_addBtn = findViewById(R.id.menu_add);
+        menu_addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), Add.class);
+                startActivity(startIntent);
+            }
+        });
+
+        Button menu_graphBtn = findViewById(R.id.menu_graph);
+        menu_graphBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), Graph.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
