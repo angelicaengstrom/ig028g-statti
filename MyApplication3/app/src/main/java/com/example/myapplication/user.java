8e9ecdf06cca8b505bad1fde7c301da79beb98ba
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2021-03-23 17:48:47 +0100

    Create user.java

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/user.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+public class user {
+    public String fullName, age , Email;
+
+    public user(){
+
+    }
+    public user(String fullName, String age, String Email){
+        this.fullName = fullName;
+        this.age = age;
+        this.Email = Email;
+    }
+}
