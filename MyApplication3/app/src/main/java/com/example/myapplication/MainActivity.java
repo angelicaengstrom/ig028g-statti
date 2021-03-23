54693256285cafa63dcd98cdf2849249584e1cdc
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2021-03-23 20:30:29 +0100

    Update MainActivity.java

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,28 +1,35 @@
+//Förlåt Jag märkte inte att du skrev kod här
+// när jag ändrade hela sidan :(
+
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register= (TextView) findViewById(R.id.register);
 
-        Button loginBtn = findViewById(R.id.loginBtn);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(this);
+    }
 
-            @Override
-            public void onClick(View v) {
-                //Gör ifsats om inlog korrekt => login
-                Intent startIntent = new Intent(getApplicationContext(), Add.class);
-                startActivity(startIntent);
-            }
-        });
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, registerUser.class));
+                break;
+
+        }
     }
 }
