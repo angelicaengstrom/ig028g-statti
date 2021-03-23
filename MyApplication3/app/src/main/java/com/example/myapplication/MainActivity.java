5ae9443caa80d21bb9e215bb899b9d69f45253a9
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2021-03-23 20:58:58 +0100

    Update MainActivity.java

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -54,11 +55,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             editTextEmail.requestFocus();
             return;
         }
+        if(Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Du har skrivit fel email, var snäll försök igen");
+            editTextEmail.requestFocus();
+            return;
+        }
         if (password.isEmpty()) {
             editTextPassword.setError("Du måste skriva lösenord");
             editTextPassword.requestFocus();
             return;
-            
+
         }
         }
     }
