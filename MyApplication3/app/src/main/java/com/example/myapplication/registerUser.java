077cae9bb001cf05f808e4c918b018e9ae8306d8
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2021-03-23 20:43:53 +0100

    Update registerUser.java

--- MyApplication3/app/src/main/java/com/example/myapplication/registerUser.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/registerUser.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.UserHandle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -19,8 +18,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.regex.Pattern;
 
 public class registerUser extends AppCompatActivity implements View.OnClickListener {
     private TextView banner, registerUser;
@@ -42,7 +39,7 @@ public class registerUser extends AppCompatActivity implements View.OnClickListe
 
         editTextFullName = (EditText) findViewById(R.id.fullname);
         editTextAge = (EditText) findViewById(R.id.age);
-        editTextEmail = (EditText) findViewById(R.id.Email);
+        editTextEmail = (EditText) findViewById(R.id.email);
         editTextPassword = (EditText) findViewById(R.id.password);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
