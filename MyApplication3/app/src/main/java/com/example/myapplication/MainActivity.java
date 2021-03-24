a83453b5debaa584afb912173e1b192a0d239f0b
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2021-03-24 20:26:11 +0100

    Update MainActivity.java

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             editTextEmail.requestFocus();
             return;
         }
-        if(Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
             editTextEmail.setError("Du har skrivit fel email, var snäll försök igen");
             editTextEmail.requestFocus();
             return;
