94ef7edc37c87f78a2a21575802b6435f0b63cb7
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-03-25 18:17:16 +0100

    Update Settings.java

--- MyApplication3/app/src/main/java/com/example/myapplication/Settings.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Settings.java
@@ -8,17 +8,20 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Settings extends AppCompatActivity {
-    private fireBaseUser user;
+    private FirebaseUser user;
     private DatabaseReference reference;
 
     private String userID;
