6bc0494f619ffe13824c341dff9170530f94a3a3
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2021-03-25 20:01:15 +0100

    Update forgetPassword.java

--- MyApplication3/app/src/main/java/com/example/myapplication/forgetPassword.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/forgetPassword.java
@@ -5,12 +5,35 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class forgetPassword extends AppCompatActivity {
+    private EditText emailEditText;
+    private Button resetPasswordButton;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        emailEditText = (EditText) findViewById(R.id.email);
+        resetPasswordButton = (Button) findViewById(R.id.resetPassword);
+
+        auth = FirebaseAuth.getInstance();
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetPassword();
+            }
+        });
+    }
+    private void resetPassword(){
+        String email = 
     }
 }
