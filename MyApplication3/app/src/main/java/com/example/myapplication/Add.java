484a67ac3e1683cac55a09aacc2a13cb04716040
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-03-25 14:48:20 +0100

    Update Add.java

--- MyApplication3/app/src/main/java/com/example/myapplication/Add.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Add.java
@@ -18,14 +18,12 @@ public class Add extends AppCompatActivity {
     EditText otherNotesEditText;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_add);
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
         bottomNavigationView.setSelectedItemId(R.id.note);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -51,83 +49,5 @@ public class Add extends AppCompatActivity {
             }
         });
 
-        /*
-
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_menu);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new NoteFragment()).commit();
-
-     */
-
-    /*
-        Button menu_calenderBtn = findViewById(R.id.menu_calender);
-        menu_calenderBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), Calender.class);
-                startActivity(startIntent);
-            }
-        });
-
-        Button menu_graphBtn = findViewById(R.id.menu_graph);
-        menu_graphBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), Graph.class);
-                startActivity(startIntent);
-            }
-        });
-
-        Button menu_settingsBtn = findViewById(R.id.menu_settings);
-        menu_settingsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), Settings.class);
-                startActivity(startIntent);
-            }
-        });
-
-
-        Button saveNoteBtn = findViewById(R.id.saveNoteBtn);
-        saveNoteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                otherNotesEditText = findViewById(R.id.otherNotes);
-
-            }
-        });
-
-    */
     }
-
-    /*
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
-
-                    switch(item.getItemId()){
-                        case R.id.nav_note:
-                            selectedFragment = new NoteFragment();
-                            break;
-                        case R.id.nav_calender:
-                            selectedFragment = new CalenderFragment();
-                            break;
-                        case R.id.nav_graph:
-                            selectedFragment = new GraphFragment();
-                            break;
-                        case R.id.nav_settings:
-                            selectedFragment = new SettingsFragment();
-                            break;
-                    }
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
-
-                    return true;
-                }
-            };*/
 }
