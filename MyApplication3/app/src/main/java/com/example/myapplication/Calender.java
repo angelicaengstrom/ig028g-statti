85ee6be7bf97d9164adad25fcdaccaf38d143514
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-03-26 10:13:01 +0100

    Update Calender.java

--- MyApplication3/app/src/main/java/com/example/myapplication/Calender.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Calender.java
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Date;
+import java.util.List;
 
 public class Calender extends AppCompatActivity {
+    private static final String TAG = "Add";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +57,66 @@ public class Calender extends AppCompatActivity {
         });
     }
     public void readDocument(View view){
-        
+        /*
+        FirebaseFirestore.getInstance()
+                .collection("notes")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        Log.d(TAG, "onSuccess: We're getting the data");
+                        List<DocumentSnapshot> snapshotList = queryDocumentSnapshots.getDocuments();
+                        for(DocumentSnapshot snapshot: snapshotList){
+                            Log.d(TAG, "onSuccess: " + snapshot.getData().toString());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "onFailure: ", e);
+                    }
+                });*/
+        /*
+        //TA UT EN SPECIFIK DATA
+        FirebaseFirestore.getInstance()
+                .collection("notes")
+                .whereEqualTo("created", new Timestamp(new Date()))
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        Log.d(TAG, "onSuccess: We're getting the data");
+                        List<DocumentSnapshot> snapshotList = queryDocumentSnapshots.getDocuments();
+                        for(DocumentSnapshot snapshot: snapshotList){
+                            Log.d(TAG, "onSuccess: " + snapshot.getData().toString());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "onFailure: ", e);
+                    }
+                });*/
+
+        FirebaseFirestore.getInstance()
+                .collection("notes")
+                .document("4BVVZBlzaCfVY2zF9fd8")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Log.d(TAG, "onSuccess: " + documentSnapshot.getId());
+                        Log.d(TAG, "onSuccess: " + documentSnapshot.getData());
+                        Log.d(TAG, "onSuccess: " + documentSnapshot.getTimestamp("created"));
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "onFailure: ", e);
+                    }
+                });
     }
 }
