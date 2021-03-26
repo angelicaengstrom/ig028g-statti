8f826f2f619f2d1ac2675eaaf9dda476891488d7
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-03-26 19:27:57 +0100

    Update Note.java

--- MyApplication3/app/src/main/java/com/example/myapplication/Note.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Note.java
@@ -1,52 +1,74 @@
 package com.example.myapplication;
-/*
+
+import com.google.firebase.Timestamp;
+
 public class Note {
 
     private String text;
     private int feeling;
     private int trainsession;
+    private Timestamp created;
+    private String userId;
 
-    public Note(String text, int feeling, int trainsession) {
+    public Note(String text, int feeling, int trainsession, Timestamp created, String userId) {
         this.text = text;
         this.feeling = feeling;
         this.trainsession = trainsession;
+        this.created = created;
+        this.userId = userId;
 
     }
-
     public Note() {
     }
 
-    public String getText(){
+    public String getText() {
         return text;
     }
 
-    public void setText(String text){
+    public void setText(String text) {
         this.text = text;
     }
 
-    public int getFeeling(){
+    public int getFeeling() {
         return feeling;
     }
 
-    public void setFeeling(int feeling){
+    public void setFeeling(int feeling) {
         this.feeling = feeling;
     }
 
-    public int getTrainsession(int trainsession){
+    public int getTrainsession() {
         return trainsession;
     }
 
-    public void setTrainsession(int trainsession){
+    public void setTrainsession(int trainsession) {
         this.trainsession = trainsession;
     }
 
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return "Note{" +
-                "text='" + text + "\'" +
+                "text='" + text + '\'' +
                 ", feeling=" + feeling +
                 ", trainsession=" + trainsession +
-                "}";
+                ", created=" + created +
+                ", userId='" + userId + '\'' +
+                '}';
     }
 }
-*/
