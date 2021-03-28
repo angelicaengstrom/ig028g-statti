0b909cbe918be264da1b195b014daf1c0e256767
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-03-28 19:04:49 +0200

    Update Row.java

--- MyApplication3/app/src/main/java/com/example/myapplication/Row.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Row.java
@@ -1,17 +1,22 @@
 package com.example.myapplication;
 
+import java.util.List;
+
 public class Row {
 
-    private String mTitle;
+    private String title;
+    private List<String> titleItems;
 
-    public Row(String title) {
-        mTitle = title;
-
+    public Row(String title, List<String> titleItems) {
+        this.title = title;
+        this.titleItems = titleItems;
     }
 
     public String getTitle() {
-        return mTitle;
+        return this.title;
     }
+
+    public List<String> getTitleItems(){ return this.titleItems; }
 
 
 }
