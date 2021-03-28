9f4eb96d553d2c8b0bbc6b143d600022c9b8cec8
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-03-28 19:04:51 +0200

    Update RowAdapter.java

--- MyApplication3/app/src/main/java/com/example/myapplication/RowAdapter.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/RowAdapter.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -10,16 +13,31 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RowAdapter extends RecyclerView.Adapter<RowAdapter.RowViewHolder> {
     private ArrayList<Row> mRowList;
+    private static final String TAG = "RowAdapter";
 
     public static class RowViewHolder extends RecyclerView.ViewHolder{
         public TextView title;
+        public ImageView imageAdd;
+        public RecyclerView dataList;
 
         public RowViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.titleTextView);
+            imageAdd = itemView.findViewById(R.id.image_add);
+            dataList = itemView.findViewById(R.id.dataRecyclerView);
+
+            imageAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+
+            });
+
         }
     }
 
@@ -38,8 +56,11 @@ public class RowAdapter extends RecyclerView.Adapter<RowAdapter.RowViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull RowViewHolder holder, int position) {
         Row currentTitle = mRowList.get(position);
+        String titleName = currentTitle.getTitle();
+        List<String> data = currentTitle.getTitleItems();
 
-        holder.title.setText(currentTitle.getTitle());
+        holder.title.setText(titleName);
+
     }
 
     @Override
