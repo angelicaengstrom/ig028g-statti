7727a19e184cf23cb1dca5ccafb1353ca969b575
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-03-28 17:24:29 +0200

    Update Row.java

--- MyApplication3/app/src/main/java/com/example/myapplication/Row.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Row.java
@@ -1,52 +1,17 @@
 package com.example.myapplication;
 
-import com.google.firebase.Timestamp;
-
 public class Row {
 
-    private String title;
-    private String userId;
-    private Timestamp created;
+    private String mTitle;
 
-    public Row(String title, String userId, Timestamp created) {
-        this.title = title;
-        this.userId = userId;
-        this.created = created;
+    public Row(String title) {
+        mTitle = title;
 
-    }
-    public Row() {
     }
 
     public String getTitle() {
-        return title;
+        return mTitle;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public Timestamp getCreated() {
-        return created;
-    }
-
-    public void setCreated(Timestamp created) {
-        this.created = created;
-    }
-
-    @Override
-    public String toString() {
-        return "Row{" +
-                "title='" + title + '\'' +
-                "userId='" + userId + '\'' +
-                ", created=" + created +
-                '}';
-    }
 }
