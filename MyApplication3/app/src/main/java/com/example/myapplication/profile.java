ca0d3ade9800b4481547dc9da82b300fb4f28577
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2021-04-22 18:20:45 +0200

    Update profile.java

--- MyApplication3/app/src/main/java/com/example/myapplication/profile.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/profile.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class profile extends AppCompatActivity {
+    public static final String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
     }
 }
