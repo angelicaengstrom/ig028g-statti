44b6b7534595eab88ab3ac7789ee5b943c262ea8
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2021-04-24 15:22:59 +0200

    Update profile.java

--- MyApplication3/app/src/main/java/com/example/myapplication/profile.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/profile.java
@@ -2,15 +2,25 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class profile extends AppCompatActivity {
     public static final String TAG = "TAG";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Intent data = getIntent();
+        String name = data.getStringExtra("name");
+        String email = data.getStringExtra("email");
+        String age = data.getStringExtra("age");
+
+        Log.d(TAG, "onCreate: " + name + " " + email + " " + age);
 
     }
 }
