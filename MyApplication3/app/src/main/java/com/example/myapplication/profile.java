7941cf8d7dc8a118cb534b21fdec8cf90c50e6ff
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2021-04-24 15:35:41 +0200

    Update profile.java

--- MyApplication3/app/src/main/java/com/example/myapplication/profile.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/profile.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class profile extends AppCompatActivity {
     public static final String TAG = "TAG";
-
+    EditText profileFullName, profileEmail, profileAge;
+    ImageView profileImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,28 @@ public class profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         Intent data = getIntent();
-        String name = data.getStringExtra("name");
+        String fullname = data.getStringExtra("fullname");
         String email = data.getStringExtra("email");
         String age = data.getStringExtra("age");
 
-        Log.d(TAG, "onCreate: " + name + " " + email + " " + age);
+        profileFullName = findViewById(R.id.profileFullName);
+        profileEmail = findViewById(R.id.profileEmailAdress);
+        profileAge = findViewById(R.id.profileAge);
+        profileImageView = findViewById(R.id.profileImageView);
+
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Toast.makeText(profile.this,"Whatever",Toast.LENGTH_SHORT).show();
+            
+            }
+        });
+
+        profileFullName.setText(fullname);
+        profileEmail.setText(email);
+        profileAge.setText(age);
+
+        Log.d(TAG, "onCreate: " + fullname + " " + email + " " + age);
 
     }
 }
