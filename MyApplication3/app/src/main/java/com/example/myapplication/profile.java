b58c5250ffcad5448c1f211269bdbf189c287960
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2021-04-28 09:42:07 +0200

    Update profile.java

--- MyApplication3/app/src/main/java/com/example/myapplication/profile.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/profile.java
@@ -6,19 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class profile extends AppCompatActivity {
     public static final String TAG = "TAG";
     EditText profileFullName, profileEmail, profileAge;
     ImageView profileImageView;
+    Button saveBtn;
     FirebaseAuth auth;
     FirebaseFirestore fstore;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,8 @@ public class profile extends AppCompatActivity {
         profileEmail = findViewById(R.id.profileEmailAdress);
         profileAge = findViewById(R.id.profileAge);
         profileImageView = findViewById(R.id.profileImageView);
+        saveBtn = findViewById(R.id.saveInfo);
+
 
         profileImageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +49,16 @@ public class profile extends AppCompatActivity {
             Toast.makeText(profile.this,"Whatever",Toast.LENGTH_SHORT).show();
 
             }
+        });
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (profileFullName.getText().toString().isEmpty() || profileEmail.getText().toString().isEmpty() || profileAge.getText().toString().isEmpty()){
+                    Toast.makeText(profile.this, "En eller flera är tomma", Toast.LENGTH_SHORT).show();
+                return;
+            }
+                String email = profileFullName.getText().toString().isEmpty();
+        }
         });
 
         profileFullName.setText(fullname);
