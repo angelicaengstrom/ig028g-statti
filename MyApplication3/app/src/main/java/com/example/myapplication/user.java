8356eccfdca8238c2425d6d2ece1b2ae0b8c1e30
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-05-06 02:32:47 +0200

    Update user.java

--- MyApplication3/app/src/main/java/com/example/myapplication/user.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/user.java
@@ -1,15 +1,16 @@
 package com.example.myapplication;
 
 public class user {
-    public String fullName, age , Email;
+    public String fullName, age , Email, Gender;
 
     public user(){
 
     }
 
-    public user(String fullName, String age, String Email){
+    public user(String fullName, String age, String Email, String Gender){
         this.fullName = fullName;
         this.age = age;
         this.Email = Email;
+        this.Gender = Gender;
     }
 }
