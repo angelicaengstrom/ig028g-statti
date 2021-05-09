a4637bfed817f5ad6da679cdda89d9d03ca69f66
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2021-05-09 14:13:42 +0200

    Update Graph.java

--- MyApplication3/app/src/main/java/com/example/myapplication/Graph.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Graph.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -17,6 +19,24 @@ public class Graph extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        Spinner spinner = findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                .createFromResource(this, R.array.trainingType, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter
+                .createFromResource(this, R.array.date, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+        Spinner spinner3 = findViewById(R.id.spinner4);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter
+                .createFromResource(this, R.array.item, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
